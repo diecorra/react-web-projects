06a37fa1b3463d43aaa1a40425c1744be80de176
Diego Corradi <diego.corradi95@gmail.com>
2023-05-29 10:34:51 +0200

    added copyColor functionality

--- color-grading/src/components/ColorGrading.js
+++ color-grading/src/components/ColorGrading.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Values from 'values.js';
 import SingleColor from './SingleColor';
-import { v4 as uuidv4 } from 'uuid';
 
 const ColorGrading = () => {
+  const [selectedColor, setselectedColor] = useState([]);
+  const [isError, setIsError] = useState(false);
   const [colorInput, setColorInput] = useState({
     color: '',
     qty: 5,
@@ -11,46 +13,71 @@ const ColorGrading = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsError(false);
+    if (colorInput.color && colorInput.qty) {
+      const { color, qty } = colorInput;
+      try {
+        setselectedColor(
+          new Values(color).all(Math.round(100 / parseInt(qty, 10)) * 2)
+        );
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      }
+    }
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    console.log(name);
     setColorInput({
       ...colorInput,
       [name]: value,
     });
   };
 
+  useEffect(() => {
+    setColorInput({ qty: 10, color: '#919901' });
+    setselectedColor(new Values('#919901').all(Math.round(100 / 10) * 2));
+  }, []);
+
   return (
     <>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='input-group'>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
           <input
-            type='text'
-            name='color'
-            id='color'
+            type="text"
+            name="color"
+            id="color"
             value={colorInput.color}
             maxLength={7}
             onChange={handleChange}
-            className='input'
+            className="input"
           ></input>
           <input
-            type='number'
-            name='qty'
-            id='qty'
+            type="number"
+            name="qty"
+            id="qty"
             value={colorInput.qty}
             max={100}
             min={5}
             step={5}
             onChange={handleChange}
-            className='input'
+            className="input"
           ></input>
         </div>
-        <button className='btn btn-selector' type='submit'>
+        <button className="btn btn-selector" type="submit">
           Create
         </button>
       </form>
+      <section className="color-section">
+        {isError ? (
+          <h4 className="section-center">No Color Found!</h4>
+        ) : selectedColor.length > 0 ? (
+          selectedColor.map((el) => <SingleColor key={uuidv4()} {...el} />)
+        ) : (
+          <h4>Loading...</h4>
+        )}
+      </section>
     </>
   );
 };
--- color-grading/src/components/SingleColor.js
+++ color-grading/src/components/SingleColor.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { rgbToHex } from '../utils/helpers';
 
-const SingleColor = () => {
-  return <div>Single Color</div>;
+const SingleColor = ({ rgb, type, weight }) => {
+  const [message, setMessage] = useState(false);
+
+  const copyColor = () => {
+    navigator.clipboard
+      .writeText(rgbToHex(...rgb))
+      .then(() => setMessage(true))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [message]);
+
+  return (
+    <article
+      onClick={copyColor}
+      className={`single-color ${type}`}
+      style={{ backgroundColor: rgbToHex(...rgb) }}
+    >
+      <h5>{rgbToHex(...rgb)}</h5>
+      {message && <p>Color Copied!</p>}
+    </article>
+  );
 };
 
 export default SingleColor;
