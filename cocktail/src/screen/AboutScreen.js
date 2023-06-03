c7dbf6bd815f57d083da334ddbb9fa6a171a7de7
diecorra <diego.corradi95@gmail.com>
2023-06-03 12:06:23 +0200

    feat: implemented About and Error sections

--- cocktail/src/screen/AboutScreen.js
+++ cocktail/src/screen/AboutScreen.js
@@ -1,11 +1,57 @@
-import React from "react";
-import { Hero, Card } from "../components";
-import { valueCards, teamCards } from "../utils/info";
-import image from "../assets/image/about-hero.jpg";
-import teamImg from "../assets/image/team-img.jpg";
+import React from 'react';
+import { Hero, Card } from '../components';
+import { valueCards, teamCards } from '../utils/info';
+import image from '../assets/image/about-hero.jpg';
+import teamImg from '../assets/image/team-img.jpg';
 
 const AboutScreen = () => {
-  return <>About Screen</>;
+  return (
+    <>
+      <Hero img={image}>
+        <section className='about-hero'>
+          <div className='about-text'>
+            <div className='topline'></div>
+            <h3>
+              <q>
+                Great is the fortune of he who possesses a good bottle, a good
+                book, and a good friend.
+              </q>
+            </h3>
+            <div className='underline'></div>
+          </div>
+        </section>
+      </Hero>
+      <section className='about-value'>
+        <div className='container about-value-content'>
+          <h3 className='brand-secondary-color'>OUR PROJECT</h3>
+          <div className='card-section'>
+            {valueCards.map((card) => {
+              return (
+                <Card key={card.title} {...card} className={'value-card'} />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      <section className='about-team'>
+        <div
+          className='team-img'
+          style={{
+            background: `url(${teamImg})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        ></div>
+        <div className='about-team-content container'>
+          <h3 className='brand-secondary-color'>OUR TEAM</h3>
+          {teamCards.map((card) => {
+            return <Card key={card.title} {...card} />;
+          })}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default AboutScreen;
--- cocktail/src/screen/ErrorScreen.js
+++ cocktail/src/screen/ErrorScreen.js
@@ -1,18 +1,44 @@
-import React from "react";
-import styled from "styled-components";
-import Lottie from "react-lottie";
-import animationData from "../assets/animation/not-found.json";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import animationData from '../assets/animation/not-found.json';
+import { Link } from 'react-router-dom';
 const ErrorScreen = () => {
-  return <Wrapper>Error Screen</Wrapper>;
+  return (
+    <Wrapper>
+      <h3>Page Not Found</h3>
+      <Link to='/' className='btn btn-primary'>
+        Go back Home
+      </Link>
+      <Lottie
+        options={{
+          loop: true,
+          autoplay: true,
+          reverse: true,
+          animationData: animationData,
+          rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+          },
+        }}
+        width={500}
+        height={500}
+      ></Lottie>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
   min-height: 86.2vh;
   display: grid;
+  display: ms--grid;
+  display: --moz-grid;
+  display: --webkit-grid;
   place-items: center;
   h3 {
     text-transform: uppercase;
+  }
+  .btn {
+    border-radius: 22px;
   }
   @media screen and (min-width: 992px) {
     min-height: 84.5vh;
